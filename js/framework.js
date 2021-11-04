62190abe9a03b051c3d7d3f754e6f5b3b4dcf8d6
JosueSO <muziko11@hotmail.com>
2021-11-04 16:11:51 -0600

    Menu

--- js/framework.js
+++ js/framework.js
@@ -2,6 +2,11 @@ const attr_toggle = "data-toggle";
 const attr_target = "data-target";
 const attr_dismiss = "data-dismiss";
 const modal_class = "modal";
+const dropdown_class = "dropdown";
+const dropdown_menu_class = "dropdown-menu";
+const dropdown_toggle = "dropdown-toggle";
+const navbar_toggle = "navbar-toggle";
+const menu_collapse_class = "navbar-collapse";
 const show_class = "show";
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -17,6 +22,20 @@ document.addEventListener("DOMContentLoaded", function() {
         
     modal_close_buttons.forEach(element => {
         element.addEventListener("click", CloseModal);
+    });
+
+    //Dropdown del submenú
+    let dropdown_buttons = document.querySelectorAll(`.${dropdown_class} > .${dropdown_toggle}`);
+        
+    dropdown_buttons.forEach(element => {
+        element.addEventListener("click", ToggleDropdown);
+    });
+
+    //Collapse del menú
+    let collapse_menu_buttons = document.querySelectorAll(`.${navbar_toggle}`);
+        
+    collapse_menu_buttons.forEach(element => {
+        element.addEventListener("click", ToggleMenu);
     });
 });
 
@@ -39,7 +58,7 @@ function OpenModal(e) {
  * Cierra un modal
  * @param {PointerEvent} e 
  */
- function CloseModal(e) {
+function CloseModal(e) {
     //Leer el elemento que se va a ocultar
     let modal_selector = e.target.getAttribute(attr_dismiss);
 
@@ -48,4 +67,47 @@ function OpenModal(e) {
 
     //Agregar la clase para mostrar el modal
     modal.classList.remove(show_class);
+}
+
+/**
+ * Abre y cierra el submenú
+ * @param {PointerEvent} e 
+ */
+function ToggleDropdown(e) {
+    e.preventDefault();
+
+    let menu = e.target.parentNode.querySelector(`.${dropdown_menu_class}`);
+    
+    if (menu.classList.contains(show_class)) {
+        menu.classList.remove(show_class);
+    }
+    else {
+        menu.classList.add(show_class);
+    }
+}
+
+/**
+ * Abre y cierra el menú
+ * @param {PointerEvent} e 
+ */
+ function ToggleMenu(e) {
+    let menu = e.target.parentNode;
+    
+    console.log(menu);
+    if (menu.classList.contains(navbar_toggle)) {
+        menu = menu.parentNode;
+    }
+
+    console.log(menu);
+
+    menu = menu.querySelector(`.${menu_collapse_class}`);
+
+    console.log(menu);
+
+    if (menu.classList.contains(show_class)) {
+        menu.classList.remove(show_class);
+    }
+    else {
+        menu.classList.add(show_class);
+    }
 }
